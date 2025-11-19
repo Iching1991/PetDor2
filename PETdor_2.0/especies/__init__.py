52cd9b4833d1749e9ff67f383c2cb0e5bcad465f
Iching1991 <agnaldoangelico.vet@gmail.com>
2025-11-19 12:41:55 -0300

    Update __init__.py

--- PETdor_2.0/especies/__init__.py
+++ PETdor_2.0/especies/__init__.py
@@ -1,17 +1,24 @@
 # PETdor_2.0/especies/__init__.py
-...
+
+"""
+Pacote para gerenciamento de configurações de espécies.
+Define as estruturas de dados para perguntas e configurações de espécies,
+e gerencia o registro e acesso a essas configurações.
+"""
+
 from .index import ( # <-- Importa diretamente de index
     EspecieConfig,
     Pergunta,
     listar_especies,
-    buscar_especie_por_id, # This is the one!
+    buscar_especie_por_id, # <-- CORREÇÃO: Importa buscar_especie_por_id
     get_especies_nomes
 )
-...
+
+# Opcional: definir __all__ para controle de importação
 __all__ = [
     "EspecieConfig",
     "Pergunta",
     "listar_especies",
-    "buscar_especie_por_id",
+    "buscar_especie_por_id", # <-- CORREÇÃO: Expõe buscar_especie_por_id
     "get_especies_nomes"
 ]
