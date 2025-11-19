a5c29936675d5b36bfb8391eccbae149a7f434dd
Iching1991 <agnaldoangelico.vet@gmail.com>
2025-11-19 10:03:58 -0300

    Update __init__.py

--- PETdor_2.0/database/__init__.py
+++ PETdor_2.0/database/__init__.py
@@ -1,4 +1,19 @@
+"""
+Pacote de acesso ao banco de dados da aplicação PETDor.
+
+Responsável por:
+- Conexão com o banco SQLite (conectar_db)
+- Criação e migração das tabelas (criar_tabelas, migrar_banco_completo)
+- Funções de acesso a dados (models)
+"""
+
 from .connection import conectar_db
 from .migration import criar_tabelas, migrar_banco_completo
+from . import models
 
-__all__ = ["conectar_db", "criar_tabelas", "migrar_banco_completo"]
+__all__ = [
+    "conectar_db",
+    "criar_tabelas",
+    "migrar_banco_completo",
+    "models",
+]
