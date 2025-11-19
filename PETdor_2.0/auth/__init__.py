bccbd8574b10b7e1e394ac53fd8d127b76210c49
Iching1991 <agnaldoangelico.vet@gmail.com>
2025-11-19 10:56:30 -0300

    Update __init__.py

--- PETdor_2.0/auth/__init__.py
+++ PETdor_2.0/auth/__init__.py
@@ -1,27 +1,27 @@
-# PetDor/auth/__init__.py
+# PETdor_2.0/auth/__init__.py
 
-from .user import (
-    cadastrar_usuario,
-    autenticar_usuario,
-    buscar_usuario_por_id,
-    buscar_usuario_por_email,
-    atualizar_usuario,
-    alterar_senha,
-    deletar_usuario
-)
+"""
+Módulo de inicialização do pacote 'auth'.
+Expõe funcionalidades de autenticação e gerenciamento de usuários.
+"""
 
-from .password_reset import reset_password_request, reset_password
-from .email_confirmation import enviar_confirmacao_email
+# Importa os submódulos para que possam ser acessados via auth.user, auth.password_reset, etc.
+from . import user
+from . import password_reset
+from . import email_confirmation # Você tem email_confirmation.py na sua estrutura
+from . import security # Você tem security.py na sua estrutura
 
 __all__ = [
-    "cadastrar_usuario",
-    "autenticar_usuario",
-    "buscar_usuario_por_id",
-    "buscar_usuario_por_email",
-    "atualizar_usuario",
-    "alterar_senha",
-    "deletar_usuario",
-    "reset_password_request",
-    "reset_password",
-    "enviar_confirmacao_email"
+    "user",
+    "password_reset",
+    "email_confirmation", # Adicionado para expor o módulo
+    "security",           # Adicionado para expor o módulo
 ]
+
+# Se você *realmente* precisar expor funções específicas diretamente do pacote 'auth'
+# (ex: auth.cadastrar_usuario), você pode fazer isso AQUI, mas com cuidado para não criar ciclos.
+# Por exemplo, se auth.user não depende de auth.__init__ para carregar, você pode fazer:
+# from .user import cadastrar_usuario, autenticar_usuario, buscar_usuario_por_id
+# __all__.extend(["cadastrar_usuario", "autenticar_usuario", "buscar_usuario_por_id"])
+# Mas para evitar o ciclo atual, é melhor que o petdor.py importe diretamente de auth.user
+# ou auth.password_reset.
