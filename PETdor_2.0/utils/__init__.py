01771734a7c55889ddbb459f72fe1c08719158b6
Iching1991 <agnaldoangelico.vet@gmail.com>
2025-11-19 09:31:56 -0300

    Update __init__.py

--- PETdor_2.0/utils/__init__.py
+++ PETdor_2.0/utils/__init__.py
@@ -1,8 +1,5 @@
-from .pdf_generator import gerar_pdf_relatorio
-from .email_sender import enviar_email_confirmacao, enviar_email_reset
-
-__all__ = [
-    "gerar_pdf_relatorio",
-    "enviar_email_confirmacao",
-    "enviar_email_reset"
-]
+"""
+Utils do PETDOR
+"""
+# Não importar nada automaticamente aqui.
+# Importações diretas nos arquivos evitarão ciclos.
