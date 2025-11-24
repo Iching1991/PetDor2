d8c746b1b646f4ae3bf8666cd907b685c1dfb689
Iching1991 <agnaldoangelico.vet@gmail.com>
2025-11-23 23:18:28 -0300

    Update __init__.py

--- PETdor2/auth/__init__.py
+++ PETdor2/auth/__init__.py
@@ -1,7 +1,20 @@
-# PETdor2/database/__init__.py
+# PETdor2/auth/__init__.py
 """
-Pacote de banco de dados - gerencia conexões com Supabase.
+Pacote de autenticação - gerencia login, registro e segurança.
+Módulos:
+  - user: funções de verificação e cadastro de usuários
+  - security: hash de senhas e tokens JWT
+  - password_reset: recuperação de senha
+  - email_confirmation: confirmação de e-mail
 """
-from .supabase_client import get_supabase, testar_conexao
+from . import user
+from . import security
+from . import password_reset
+from . import email_confirmation
 
-__all__ = ["get_supabase", "testar_conexao"]
+__all__ = [
+    "user",
+    "security",
+    "password_reset",
+    "email_confirmation",
+]
