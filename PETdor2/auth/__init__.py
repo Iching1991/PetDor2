b6a3cda5b43493f98b84dfe2a9054731a55319eb
Iching1991 <agnaldoangelico.vet@gmail.com>
2025-11-23 22:53:25 -0300

    Update __init__.py

--- PETdor2/auth/__init__.py
+++ PETdor2/auth/__init__.py
@@ -1,17 +1,8 @@
 # PETdor2/auth/__init__.py
-"""Módulo de autenticação e segurança do PETDOR."""
-
-# Importações explícitas dos submódulos do pacote auth
+"""
+Pacote de autenticação - gerencia login, registro e segurança.
+"""
 from . import user
 from . import security
-from . import password_reset
-from . import email_confirmation
 
-
-__all__ = [
-    "user",
-    "password_reset",
-    "email_confirmation",
-    "security",
-]
-
+__all__ = ["user", "security"]
