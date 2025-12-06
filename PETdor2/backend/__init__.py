91606a3ea24fbc0270dd0f437ef0435a79748f0c
Iching1991 <agnaldoangelico.vet@gmail.com>
2025-12-05 23:42:46 -0300

    Update backend package initialization and exports

--- PETdor2/backend/__init__.py
+++ PETdor2/backend/__init__.py
@@ -1,5 +1,18 @@
 """
-Pacote backend da aplicação PETDor2.
-Mantido minimalista para evitar importações circulares.
+Pacote principal do backend do PETDor2.
+Expondo subpacotes e funções principais para importações simples.
 """
-__all__ = []
+
+from . import auth
+from . import database
+from . import especies
+from . import pages
+from . import utils
+
+__all__ = [
+    "auth",
+    "database",
+    "especies",
+    "pages",
+    "utils",
+]
