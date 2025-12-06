24d6fcd9c8db7aed0108c614ed590150aa07f764
Iching1991 <agnaldoangelico.vet@gmail.com>
2025-12-05 23:55:54 -0300

    Refactor utility package with explicit imports and exports
    Updated the utility package to include explicit imports and exports.

--- PETdor2/backend/utils/__init__.py
+++ PETdor2/backend/utils/__init__.py
@@ -1,18 +1,25 @@
 """
 Pacote utilitário do PETDor2.
-Expondo funções e helpers gerais.
+Expondo funções e helpers gerais de forma explícita.
 """
 
-from .petdor import *
-from .validators import *
-from .notifications import *
-from .utils import *
-from .config import *
+# Importações nomeadas
+from .petdor import format_pet_nome, calcular_idade_pet
+from .validators import validar_email, validar_cpf
+from .notifications import enviar_email, enviar_sms
+from .utils import gerar_token, formatar_data
+from .config import CONFIG, EMAIL_SETTINGS
 
+# Lista explícita de nomes exportados
 __all__ = [
-    "petdor",
-    "validators",
-    "notifications",
-    "utils",
-    "config",
+    "format_pet_nome",
+    "calcular_idade_pet",
+    "validar_email",
+    "validar_cpf",
+    "enviar_email",
+    "enviar_sms",
+    "gerar_token",
+    "formatar_data",
+    "CONFIG",
+    "EMAIL_SETTINGS",
 ]
