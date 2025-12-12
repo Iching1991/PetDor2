f1f28c23cadcfd63eee915d9869410858450809c
Iching1991 <agnaldoangelico.vet@gmail.com>
2025-12-11 23:51:41 -0300

    Update __init__.py

--- PETdor2/backend/utils/__init__.py
+++ PETdor2/backend/utils/__init__.py
@@ -1,8 +1,1 @@
-"""
-Pacote utils do backend.
-
-Não importa submódulos automaticamente para evitar importações circulares.
-Cada módulo deve ser importado diretamente onde for utilizado.
-"""
-
-__all__ = []
+from .email_sender import enviar_email
