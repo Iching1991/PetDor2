c511878ffd401d19ca454f2e4dc44a06ababd24d
Iching1991 <agnaldoangelico.vet@gmail.com>
2026-01-26 14:23:07 -0300

    Refactor imports and __all__ definition in init.py

--- PETdor2/backend/database/__init__.py
+++ PETdor2/backend/database/__init__.py
@@ -1,21 +1,17 @@
 # backend/database/__init__.py
 
 from .supabase_client import (
-    get_supabase_client,
-    get_headers_with_jwt,
-    testar_conexao,
     supabase_table_select,
     supabase_table_insert,
     supabase_table_update,
     supabase_table_delete,
+    testar_conexao,
 )
 
 __all__ = [
-    "get_supabase_client",
-    "get_headers_with_jwt",
-    "testar_conexao",
     "supabase_table_select",
     "supabase_table_insert",
     "supabase_table_update",
     "supabase_table_delete",
+    "testar_conexao",
 ]
