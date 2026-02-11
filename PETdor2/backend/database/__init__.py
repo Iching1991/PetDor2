b19f8360d2b780833521789ae88ac6b10a868357
Iching1991 <agnaldoangelico.vet@gmail.com>
2026-02-10 22:24:03 -0300

    Refactor __init__.py to include supabase client imports

--- PETdor2/backend/database/__init__.py
+++ PETdor2/backend/database/__init__.py
@@ -2,6 +2,13 @@
 Backend PETDor2
 Inicialização leve (sem imports circulares)
 """
+from .supabase_client import (
+    supabase_table_select,
+    supabase_table_insert,
+    supabase_table_update,
+    supabase_table_delete,
+    testar_conexao,
+)
 
 # Não importar módulos inteiros aqui
 # Apenas expor namespaces se necessário
@@ -12,3 +19,4 @@ __all__ = [
     "utils",
     "especies",
 ]
+
